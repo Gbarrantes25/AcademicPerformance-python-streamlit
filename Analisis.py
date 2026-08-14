1c626d889042f1ab74fc40d8d85ca5af923e4048
Gian Barrantes <129077296+Gbarrantes25@users.noreply.github.com>
2026-08-14 17:00:34 -0500

    Update Analisis.py

--- Analisis.py
+++ Analisis.py
@@ -5,7 +5,7 @@ import numpy as np
 import streamlit as st
 import inspect
 
-st.header("Proyecto Análisis de Datos 💻", width="stretch", text_alignment="center")
+st.header("Academic Perfomance 💻", width="stretch", text_alignment="center")
 st.markdown(
     """<p>El objetivo de este proyecto es elaborar un repositorio estructurado para el análisis del rendimiento académico y posterior visualización de datos.</p> <p>Librerías a usar: numpy, pandas, matplotlib, seaborn y streamlit.</p>
     <p>Cada salida estará acompañada de su código fuente.</p>
